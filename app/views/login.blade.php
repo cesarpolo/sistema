a2c6dd60b835567a603025342f8795f1028c797a
nelboullon <nboullon@gmail.com>
2014-11-17 17:17:27 -0500

    Cambio en la vista de login

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Login</title>
+        {{HTML::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css')}}
     </head>
     <body>
         {{-- Preguntamos si hay algún mensaje de error y si hay lo mostramos  --}}
@@ -16,7 +17,19 @@
             {{ Form::password('password'); }}
             {{ Form::label('lblRememberme', 'Recordar contraseña') }}
             {{ Form::checkbox('rememberme', true) }}
-            {{ Form::submit('Enviar') }}
+            {{ Form::submit('Enviar', array('class' => 'btn btn-primary')) }}
         {{ Form::close() }}
+
+
+        <!-- Script de AngularJS -->
+        {{HTML::script('https://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js')}}
+
+        <!-- Script de jQuery -->
+        {{HTML::script('https://code.jquery.com/jquery-1.11.0.min.js')}}
+
+        <!-- Script de Bootsrap JS -->
+        {{HTML::script('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js')}}
+
+
     </body>
 </html>
